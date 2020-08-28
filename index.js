213746f49700ab34b4594cce0abc90dadc964a29
karthik chinnasamy <kchinnasamy@wealthengine.com>
2020-08-28 15:44:21 +0530

    no cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const cors = require("cors");
+// const cors = require("cors");
 const Speakeasy = require("speakeasy");
 
 const User = require("./models/UserModel");
